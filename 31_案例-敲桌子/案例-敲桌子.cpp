b052d93115e700af45d89ec3053af630177bb79f
chu1009 <zhu20061009@gmail.com>
2026-02-26 01:58:08 +0800

    Create 案例-敲桌子.cpp

---
+++ 31_案例-敲桌子/案例-敲桌子.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	for (int num = 1; num <= 100; num++) {
+		if (num % 10 == 7 || num / 10 % 10 == 7 || num % 7 == 0) {
+			std::cout << "敲桌子" << std::endl;
+		}
+		else {
+			std::cout << num << std::endl;
+		}
+	}
+	std::cin.get();
+	return 0;
+}
