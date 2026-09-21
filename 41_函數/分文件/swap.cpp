4b4b56bb9b9db602b665cfaacf3337fe68df7022
chu1009 <zhu20061009@gmail.com>
2026-09-22 00:20:51 +0800

    Create swap.cpp

---
+++ 41_函數/分文件/swap.cpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "swap.h"
+void swap(int a, int b) {
+	int temp = a;
+	a = b;
+	b = temp;
+
+	std::cout << "a = " << a << std::endl;
+	std::cout << "b = " << b << std::endl;
+}
